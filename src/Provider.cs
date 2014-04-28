d1861cd65cdd4581344a4a501841f551977077f5
beefarino <jimchristopher@gmail.com>
2014-04-28 17:25:12 -0400

    initial commit of ast provider inspired by Jason Shirk's talk at PSHSummit

---
+++ src/Provider.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Management.Automation.Language;
+using System.Management.Automation.Provider;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using CodeOwls.PowerShell.Paths;
+using CodeOwls.PowerShell.Paths.Processors;
+using CodeOwls.PowerShell.Provider;
+using CodeOwls.PowerShell.Provider.PathNodes;
+
+namespace AstProvider
+{
+    [CmdletProvider("AST", ProviderCapabilities.ShouldProcess)]
+    public class AstProvider : Provider
+    {
+        protected override IPathResolver PathResolver
+        {
+            get
+            {
+                return new AstPathResolver();
+            }
+        }
+    }
+
+
+    public class AstPathResolver : PathResolverBase
+    {
+        private string _script;
+        private ScriptBlockAst _ast;
+
+        public AstPathResolver()
+        {
+            
+        }
+
+        public override IEnumerable<IPathNode> ResolvePath(CodeOwls.PowerShell.Provider.PathNodeProcessors.IProviderContext providerContext, string path)
+        {
+            if (null == _script)
+            {
+                var script = Regex.Replace( path, @"^(.+?\.ps1).*$", "$1");
+                _script = script;
+
+                Token[] _tokens;
+                ParseError[] _errors;
+                _ast = Parser.ParseFile(_script, out _tokens, out _errors);
+            }
+            path = path.Replace(_script, String.Empty);
+            return base.ResolvePath(providerContext, path);
+        }
+        protected override IPathNode Root
+        {
+            get { return new AstPathNode(_ast, 0); }
+        }
+    }
+
+    abstract public class AstPathNodeBase : PathNodeBase
+    {
+
+        
+        private readonly List<Ast> _children = new List<Ast>(); 
+        public void AddChild(Ast childAst)
+        {
+            _children.Add( childAst );
+        }
+
+        protected List<Ast> Children
+        {
+            get { return _children; }
+        }
+
+    }
+       
+    public class AstPathNode : AstPathNodeBase
+    {
+        private readonly Ast _ast;
+        private readonly int _index;
+
+        public AstPathNode(string script)
+        {
+            _index = 0;
+            Token[] _tokens;
+            ParseError[] _errors;
+            _ast = Parser.ParseInput(script, out _tokens, out _errors);
+        }
+
+        public AstPathNode(Ast ast, int index)
+        {
+            _ast = ast;
+            _index = index;
+        }
+
+        public override IPathValue GetNodeValue()
+        {
+            var pso = PSObject.AsPSObject(_ast);
+            pso.Properties.Add( new PSNoteProperty("AstType", _ast.GetType().Name));
+            return new ContainerPathValue( _ast, Name);
+        }
+
+        public override string Name
+        {
+            get { return "AstNode" + _index.ToString(); }
+        }
+
+        public override IEnumerable<IPathNode> GetNodeChildren(CodeOwls.PowerShell.Provider.PathNodeProcessors.IProviderContext providerContext)
+        {
+            var index = 0;
+            Children.Clear();
+            var visitor = new SimpleVisitor(this);
+            _ast.Visit(visitor);
+            return from child in Children 
+                   select new AstPathNode(child, ++index);
+        }
+    }
+}
---
+++ src/SimpleVisitor.cs
@@ -0,0 +1,296 @@
+﻿using System.Management.Automation.Language;
+
+namespace AstProvider
+{
+    class SimpleVisitor : AstVisitor
+    {
+        private readonly AstPathNodeBase _collector;
+
+        public SimpleVisitor( AstPathNodeBase ast)
+        {
+            _collector = ast;
+        }
+
+        private bool _first = true;
+        AstVisitAction DoNextAction(Ast ast) 
+        {
+            if (_first)
+            {
+                _first = false;
+                return AstVisitAction.Continue;
+            }
+            _collector.AddChild(ast);
+            return AstVisitAction.SkipChildren;
+        }
+
+        public override AstVisitAction VisitErrorStatement(ErrorStatementAst ast)
+        {
+
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitErrorExpression(ErrorExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitScriptBlock(ScriptBlockAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitParamBlock(ParamBlockAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitNamedBlock(NamedBlockAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitTypeConstraint(TypeConstraintAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitAttribute(AttributeAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitParameter(ParameterAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitTypeExpression(TypeExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitFunctionDefinition(FunctionDefinitionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitStatementBlock(StatementBlockAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitIfStatement(IfStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitTrap(TrapStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitSwitchStatement(SwitchStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitDataStatement(DataStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitForEachStatement(ForEachStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitDoWhileStatement(DoWhileStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitForStatement(ForStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitWhileStatement(WhileStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitCatchClause(CatchClauseAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitTryStatement(TryStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitBreakStatement(BreakStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitContinueStatement(ContinueStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitReturnStatement(ReturnStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+        public override AstVisitAction VisitExitStatement(ExitStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitThrowStatement(ThrowStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitDoUntilStatement(DoUntilStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitAssignmentStatement(AssignmentStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitPipeline(PipelineAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitCommand(CommandAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitCommandExpression(CommandExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitCommandParameter(CommandParameterAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitMergingRedirection(MergingRedirectionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitFileRedirection(FileRedirectionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitBinaryExpression(BinaryExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitUnaryExpression(UnaryExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitConvertExpression(ConvertExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitConstantExpression(ConstantExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitStringConstantExpression(StringConstantExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitSubExpression(SubExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitUsingExpression(UsingExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitVariableExpression(VariableExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitMemberExpression(MemberExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitInvokeMemberExpression(InvokeMemberExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitArrayExpression(ArrayExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitArrayLiteral(ArrayLiteralAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitHashtable(HashtableAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitScriptBlockExpression(ScriptBlockExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitParenExpression(ParenExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitExpandableStringExpression(ExpandableStringExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitIndexExpression(IndexExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitAttributedExpression(AttributedExpressionAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitBlockStatement(BlockStatementAst ast)
+        {
+            return DoNextAction(ast);
+        }
+
+        public override AstVisitAction VisitNamedAttributeArgument(NamedAttributeArgumentAst ast)
+        {
+            return DoNextAction(ast);
+        }
+    }
+}
